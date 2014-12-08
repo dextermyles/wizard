f9828829f2d989652daa037cc398ffccb9423f7d
dexsauce <dexter.brock@gmail.com>
2014-12-08 16:51:00 -0500

    added hand history to sql delete

--- Web/clear_game_data.sql
+++ Web/clear_game_data.sql
@@ -2,3 +2,4 @@ delete from GameLobby WHERE GameLobby.GameLobbyId IN (SELECT GameLobbyId FROM Ga
 delete from GameLobbyPlayers WHERE GameLobbyPlayers.GameLobbyId IN (SELECT GameLobbyId FROM Game WHERE DateCompleted IS NULL);
 delete from GamePlayers WHERE GamePlayers.GameId IN (SELECT GameId FROM Game WHERE DateCompleted IS NULL);
 delete from Game WHERE Game.DateCompleted IS NULL;
+delete from HandHistory;
