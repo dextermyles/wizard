4bb6e0f612fa35df3b95aaadcd92420c68882ef5
dexsauce <dexter.brock@gmail.com>
2014-12-08 16:49:45 -0500

    sql updates

--- Web/clear_game_data.sql
+++ Web/clear_game_data.sql
@@ -1,4 +1,4 @@
-delete from Game WHERE Game.DateCompleted IS NULL;
-delete from GamePlayers WHERE GamePlayers.GameId IN (SELECT GameId FROM Game WHERE DateCompleted IS NULL);
 delete from GameLobby WHERE GameLobby.GameLobbyId IN (SELECT GameLobbyId FROM Game WHERE DateCompleted IS NULL);
 delete from GameLobbyPlayers WHERE GameLobbyPlayers.GameLobbyId IN (SELECT GameLobbyId FROM Game WHERE DateCompleted IS NULL);
+delete from GamePlayers WHERE GamePlayers.GameId IN (SELECT GameId FROM Game WHERE DateCompleted IS NULL);
+delete from Game WHERE Game.DateCompleted IS NULL;
