28f9cd53673a2678c60e563584cf21bb1c7db44b
dexsauce <dexter.brock@gmail.com>
2014-12-10 01:27:10 -0500

    hand history support

---
+++ Web/ViewHandHistory.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="ViewHandHistory.aspx.cs" Inherits="WizardGame.ViewHandHistory" %>
+<asp:Content ID="ContentHead" ContentPlaceHolderID="HeadContent" runat="server">
+    <!-- viewport settings for mobile -->
+    <meta name="viewport" content="height=device-height,width=device-width,initial-scale=0.45,maximum-scale=1" >
+</asp:Content>
+<asp:Content ID="ContentMain" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="page-header hidden-xs" style="margin-top: 14px;">
+            <h1>Hand History</h1>
+        </div>
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <strong>
+                        GameID: #<%=GameId %>
+                        <span class="pull-right">Completed on <%=GameData.DateCompleted.Value.ToString("d") %></span>
+                    </strong>
+                    <div class="clearfix"></div>
+                </div>
+                <table class="table table-striped table-responsive table-hover hand-history">
+                    <thead>
+                        <tr>
+                            <th class="text-center">Hand #</th>
+                            <th class="text-center">Round</th>
+                            <th class="text-center hidden-xs">Cards Played</th>
+                            <th class="text-center">Trump</th>
+                            <th class="text-center">Suit to follow</th>
+                            <th class="text-center">Winner</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <%=HandHistoryHtml() %>
+                    </tbody>
+                </table>
+                <style type="text/css">
+                    .hand-history tr td {
+                        vertical-align: middle !important;
+                    }
+                    .card-owner {
+                        background-color: rgb(199, 199, 199);
+                        border-radius: 5px;
+                    }
+
+                    .pagination {
+                        display: inline-block;
+                        padding-left: 0;
+                        margin: 0;
+                        border-radius: 4px;
+                    }
+                </style>
+                <div class="panel-footer text-center">
+                    <a href="Home.aspx">Back to dashboard</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Web/ViewHandHistory.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WizardGame.Services;
+using WizardGame.Helpers;
+using System.Text;
+
+namespace WizardGame
+{
+    public partial class ViewHandHistory : System.Web.UI.Page
+    {
+        public Session UserSession = null;
+        public Player[] UserPlayers = null;
+        public User UserData = null;
+        public Game GameData = null;
+        public int GameId = 0;
+
+        // service
+        WizardService wizWS = new WizardService();
+
+        protected override void OnLoad(EventArgs e)
+        {
+            // is valid session
+            bool isValidSession = Functions.IsValidSession();
+
+            if (!isValidSession)
+            {
+                // redirect to login page
+                Response.Redirect("~/Default.aspx?Error=Session is not valid");
+            }
+
+            base.OnLoad(e);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // get user session data
+            UserSession = Functions.GetSessionFromCookie();
+
+            // get game id from query string
+            string strGameId = Request.QueryString["GameId"];
+
+            // game id exists
+            if (!string.IsNullOrEmpty(strGameId))
+            {
+                // parse game id
+                int.TryParse(strGameId, out GameId);
+            }
+
+            // game exists
+            if (GameId > 0)
+            {
+                GameData = wizWS.GetGameById(GameId);
+            }
+        }
+
+        public string HandHistoryHtml()
+        {
+            StringBuilder html = new StringBuilder();
+
+            // game id is set
+            if (GameId > 0)
+            {
+                // handhistory array
+                HandHistory[] handHistory = wizWS.GetHandHistoryByGameId(GameId);
+                Player[] gamePlayers = wizWS.ListPlayersByGameId(GameId);
+
+                // hand history exists
+                if (handHistory != null && handHistory.Length > 0)
+                {
+                    int handNum = 1;
+
+                    // loop through hand history
+                    for (int i = 0; i < handHistory.Length; i++)
+                    {
+                        // hand reference
+                        HandHistory history = handHistory[i];
+                        
+                        // winning player
+                        Player winningPlayer = wizWS.GetPlayerById(history.WinnerPlayerId);
+
+                        // cards played
+                        string cardsPlayed = string.Empty;
+
+                        cardsPlayed += "<ul class='pagination'>";
+
+                        // append cards played
+                        for(int x = 0; x < history.CardsPlayed.Length; x++) {
+                            // card ref
+                            Card cardPlayed = history.CardsPlayed[x];
+
+                            // card image
+                            string cardImage = cardPlayed.GetImagePath();
+
+                            // replace physical path
+                            cardImage = cardImage.Replace(Request.ServerVariables["APPL_PHYSICAL_PATH"], String.Empty);
+
+                            string ownerClass = "";
+
+                            if (cardPlayed.OwnerPlayerId == winningPlayer.PlayerId)
+                                ownerClass = "active";
+
+                            cardsPlayed += "<li class='" + ownerClass + "'><a class='card-owner'><div>" + gamePlayers.FirstOrDefault(gp => gp.PlayerId == cardPlayed.OwnerPlayerId).Name + "</div><img src='" + cardImage + "' class='img-thumbnail' style='height: 96px;'/></a></li>";  
+                        }
+
+                        cardsPlayed += "</ul>";
+
+                        // trump card image
+                        string trumpCardImage = history.TrumpCard.GetImagePath();
+
+                        // replace physical path
+                        trumpCardImage = trumpCardImage.Replace(Request.ServerVariables["APPL_PHYSICAL_PATH"], String.Empty);
+
+                        // suit to follow image
+                        string suitToFollowImage = "/Assets/Cards/" + history.SuitToFollow.ToString() + "_0.png";
+
+                        // fluff if no suit to follow
+                        if (history.SuitToFollow == Suit.None)
+                            suitToFollowImage = "/Assets/Cards/fluff.png";
+
+                        html.AppendLine("<tr>");
+                        html.AppendLine("<td class=\"text-center\">" + handNum + "</td>");
+                        html.AppendLine("<td class=\"text-center\">" + history.Round + "</td>");
+                        html.AppendLine("<td class=\"text-center hidden-xs\">" + cardsPlayed + "</td>");
+                        html.AppendLine("<td class=\"text-center\"><img src='" + trumpCardImage + "' class='img-thumbnail' style='height: 96px;' /></td>");
+                        html.AppendLine("<td class=\"text-center\"><img src='" + suitToFollowImage + "' class='img-thumbnail' style='height: 96px;' /></td>");
+                        html.AppendLine("<td class=\"text-center\">" + winningPlayer.Name + "</td>");
+                        html.AppendLine("</tr>");
+
+                        // increment hand #
+                        handNum++;
+                    }
+                }
+                else
+                {
+                    html.AppendLine("<tr><td colspan='6'>Sorry! Hand history is not available for this game</tr>");
+                }
+            }
+
+            return html.ToString();
+        }
+    }
+}
